703fc8643c39f63732bd4b3737b6823231a1518d
Tannier Yannis <tannier.yannis@gmail.com>
2017-11-03 16:07:30 +0100

    pass worker ton prod

--- settings/worker.py
+++ settings/worker.py
@@ -9,3 +9,14 @@ INSTALLED_APPS += [
     #Own apps
     'apps.worker.apps.WorkerConfig',
 ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': get_env_variable('DJANGO_DB_PROD_HOST'),
+        'NAME': get_env_variable('DJANGO_DB_PROD_NAME'),
+        'USER': get_env_variable('DJANGO_DB_PROD_USER'),
+        'PASSWORD': get_env_variable('DJANGO_DB_PROD_PASSWORD'),
+    }
+}
